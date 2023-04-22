7a9f33ef0bde70d074cc4672dbbccad2ee683cec
Aleksey Ivanov <16mln@mail.ru>
2023-04-22 17:01:39 +0300

    добавил задачу шпаргалку 4

--- task25/Program.cs
+++ task25/Program.cs
@@ -5,3 +5,14 @@
 
 // 2, 4 -> 16
 
+Console.WriteLine("Введите число A : ");
+int A = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+int i = 1;
+while (i <= N)
+{
+    factorial = factorial * i;
+    i++;
+}
+
+Console.WriteLine(factorial);
